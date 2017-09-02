4214a8ef15ba6c1630f347648d40a04aff32832c
Hyago Goularte <hyagogoularte@gmail.com>
2017-09-02 18:58:54 -0300

    Ajustado configuração e corrigido bug de não listar convites recebidos e contatos

--- perfis/urls.py
+++ perfis/urls.py
@@ -4,5 +4,8 @@ from perfis import views as perfis_views
 
 urlpatterns = [
     url(r'^$', perfis_views.index, name='index'),
-    url(r'^perfis/(?P<perfil_id>\d+)$', perfis_views.exibir, name='exibir')
+    url(r'^perfis/(?P<perfil_id>\d+)$', perfis_views.exibir, name='exibir'),
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', perfis_views.convidar, name='convidar'),
+    url(r'^convite/(?P<convite_id>\d+)/aceitar$', perfis_views.aceitar, name='aceitar')
+
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
-from perfis.models import Perfil
+from django.shortcuts import render, redirect
+from perfis.models import Perfil, Convite
 
 def index(request):
-    return render(request, 'index.html')
+    perfis_obj = Perfil.objects.all()
+    perfil_logado_obj = get_perfil_logado(request)
+    return render(request, 'index.html', {"perfis": perfis_obj, "perfil_logado": perfil_logado_obj})
 
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
+    
+    perfil_logado_obj = get_perfil_logado(request)
+    ja_eh_contato = perfil in perfil_logado_obj.contatos.all()
+    
+    return render(request, 'perfil.html', {"perfil": perfil, "ja_eh_contato": ja_eh_contato})
 
+def convidar(request, perfil_id):
+    perfil_a_convidar_obj = Perfil.objects.get(id=perfil_id)
+    perfil_logado_obj = get_perfil_logado(request)
+    perfil_logado_obj.convidar(perfil_a_convidar_obj)
+    return redirect('index')
 
-    return render(request, 'perfil.html', {"perfil": perfil})
+def aceitar(request, convite_id):
+    convite_obj = Convite.objects.get(id=convite_id)
+    convite_obj.aceitar()
+    return redirect('index')
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1)
